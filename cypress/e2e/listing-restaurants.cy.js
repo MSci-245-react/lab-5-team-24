c91d61a61b8e177eaf3e865be4091e627fd1c067
aryaaghakoochek <68356114+aryaaghakoochek@users.noreply.github.com>
2024-03-18 18:09:51 +0000

    e2e update

--- cypress/e2e/listing-restaurants.cy.js
+++ cypress/e2e/listing-restaurants.cy.js
@@ -1,11 +1,27 @@
+// describe('Listing Restaurants', () => {
+//     it('shows restaurants from the server', () => {
+//   const sushiPlace = 'Sushi Place'; const pizzaPlace = 'Pizza Place';
+//       cy.intercept('GET', 'https://api.outsidein.dev/OmPRnBiM7o7qHdTLQ7nmXUT5rVirG6ES/restaurants', [
+//         {id: 1, name: sushiPlace},
+//         {id: 2, name: pizzaPlace},
+//   ]);
+//       cy.visit('/');
+//       cy.contains(sushiPlace);
+//       cy.contains(pizzaPlace);
+//   }); 
+// });
+
 describe('Listing Restaurants', () => {
-    it('shows restaurants from the server', () => {
-  const sushiPlace = 'Sushi Place'; const pizzaPlace = 'Pizza Place';
-      cy.intercept('GET', 'https://api.outsidein.dev/OmPRnBiM7o7qHdTLQ7nmXUT5rVirG6ES/restaurants', [
-        {id: 1, name: sushiPlace},
-        {id: 2, name: pizzaPlace},
-  ]);
-      cy.visit('/');
-      cy.contains(sushiPlace);
-      cy.contains(pizzaPlace);
-  }); });
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+    cy.intercept('GET', 'https://api.outsidein.dev/*/restaurants', [
+      { id: 1, name: sushiPlace },
+      { id: 2, name: pizzaPlace },
+    ]);
+    
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
