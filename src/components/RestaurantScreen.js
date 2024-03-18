c5a369733c6387ca46e1c0d97f77298dd0507a7d
aryaaghakoochek <68356114+aryaaghakoochek@users.noreply.github.com>
2024-03-18 17:22:39 +0000

    lab 10

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -2,10 +2,9 @@ import RestaurantList from './RestaurantList';
 
 export default function RestaurantScreen() {
     return (
-    <div> 
-        <h1>Restaurants</h1>
-        <RestaurantList />
-    </div>
-    ); 
+        <div>
+            <h1>Restaurants</h1>
+            <RestaurantList />
+        </div>
+    );
 }
-  
